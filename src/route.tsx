db3c85bee2bc356bec332c71f6544da0e00b93c8
Zuma <zuma@zuma.eu.org>
2024-09-20 00:07:01 +0100

    Added documentation to router and route classes

--- src/route.tsx
+++ src/route.tsx
@@ -3,9 +3,21 @@ import { Router } from "./router";
 
 export type Layout = FC<PropsWithChildren<{}>>;
 
+/**
+ * A component and the path it should be rendered on
+ */
 export class Route<Path extends string = string> {
+  /**
+   * A react node that should be rendered on the route
+   */
   public readonly children: ReactNode;
 
+  /**
+   * @param router The router the route is assigned to
+   * @param path The path to the route
+   * @param ViewComponent The component to render on the route
+   * @param layouts Any layout components to wrap the view component
+   */
   constructor(
     private readonly router: Router,
     public readonly path: Path,
@@ -18,6 +30,13 @@ export class Route<Path extends string = string> {
     );
   }
 
+  /**
+   * Create a route that inherits this route's path and layouts
+   * @param pathExtension The extension to the inherited path
+   * @param ViewComponent The component to render on the route
+   * @param layouts Any layout components to wrap the view component
+   * @returns The created route instance
+   */
   extendRoute<PathExtension extends string>(
     pathExtension: PathExtension,
     ViewComponent: FC,
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,19 @@
 import { FC } from "react";
 import { Layout, Route } from "./route";
 
+/**
+ * A collection of routes that make up an application
+ */
 export class Router {
   private routes: Route[] = [];
 
+  /**
+   * Create a route and add it to the router
+   * @param path The path to the route
+   * @param ViewComponent The component to render on the route
+   * @param layouts Any layout components to wrap the view component
+   * @returns The created route instance
+   */
   createRoute<Path extends string>(
     path: Path,
     ViewComponent: FC,
