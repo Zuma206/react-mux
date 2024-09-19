b092a5fd0be53d395ad39557713959762c534e1f
Zuma <zuma@zuma.eu.org>
2024-09-20 00:00:54 +0100

    Added route creation systems

--- src/route.tsx
+++ src/route.tsx
@@ -1,1 +1,32 @@
-export class Route {}
+import { FC, PropsWithChildren, ReactNode } from "react";
+import { Router } from "./router";
+
+export type Layout = FC<PropsWithChildren<{}>>;
+
+export class Route<Path extends string = string> {
+  public readonly children: ReactNode;
+
+  constructor(
+    private readonly router: Router,
+    public readonly path: Path,
+    ViewComponent: FC,
+    private readonly layouts: Layout[]
+  ) {
+    this.children = layouts.reduce(
+      (view, Layout) => <Layout>{view}</Layout>,
+      <ViewComponent />
+    );
+  }
+
+  extendRoute<PathExtension extends string>(
+    pathExtension: PathExtension,
+    ViewComponent: FC,
+    ...layouts: Layout[]
+  ) {
+    return this.router.createRoute(
+      `${this.path}${pathExtension}`,
+      ViewComponent,
+      ...layouts
+    );
+  }
+}
--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,16 @@
-import { Route } from "./route";
+import { FC } from "react";
+import { Layout, Route } from "./route";
 
 export class Router {
   private routes: Route[] = [];
+
+  createRoute<Path extends string>(
+    path: Path,
+    ViewComponent: FC,
+    ...layouts: Layout[]
+  ) {
+    const route = new Route(this, path, ViewComponent, layouts);
+    this.routes.push(route);
+    return route;
+  }
 }
